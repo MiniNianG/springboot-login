6b3aeebca97d79a4df7f0f648fc1162bcd51d999
MiniNianG <528717605@qq.com>
2021-10-14 09:45:07 +0800

    第三次idea提交git测试

--- src/main/java/edu/sy/LoginApplication.java
+++ src/main/java/edu/sy/LoginApplication.java
@@ -10,5 +10,6 @@ public class LoginApplication {
     public static void main(String[] args) {
         SpringApplication.run(LoginApplication.class,args);
         /*2222222*/
+        /*3333333*/
     }
 }
